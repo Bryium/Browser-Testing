362bcad5f09c4bb633210389c7588952c8a54a6f
Bryium <bryiumonyancha@gmail.com>
2024-05-17 00:01:24 +0300

    changed the whole code structure

--- Test.py
+++ Test.py
@@ -1,45 +1,47 @@
 import os
 import pathlib
 import unittest
-
-
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 def file_url(filename):
     return pathlib.Path(os.path.abspath(filename)).as_uri()
 
-
-
-driver = webdriver.Chrome()
-
+# Set up the WebDriver
+driver = webdriver.Chrome()  # Make sure chromedriver is in your PATH or provide the executable_path
 
 class WebpageTests(unittest.TestCase):
     
-    def test_title(self):
-        driver.get(file_url("counter.html"))
-        self.assertEqual(driver.title, "Counter")
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = driver
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+    def test_title(self):
+        self.driver.get(file_url("counter.html"))
+        self.assertEqual(self.driver.title, "Counter")
 
     def test_increase(self):
-        driver.get(file_url("counter.html"))
-        increase = driver.find_element_by_id("increase")
+        self.driver.get(file_url("counter.html"))
+        increase = self.driver.find_element(By.ID, "increase")
         increase.click()
-        self.assertEqual(driver.find_element_by_tag_name("h1").text, "1")
+        self.assertEqual(self.driver.find_element(By.TAG_NAME, "h1").text, "1")
 
     def test_decrease(self):
-        driver.get(file_url("counter.html"))
-        decrease = driver.find_element_by_id("decrease")    
+        self.driver.get(file_url("counter.html"))
+        decrease = self.driver.find_element(By.ID, "decrease")
         decrease.click()
-        self.assertEqual(driver.find_elemnt_by_tag_name("h1").text, "-1")
-
+        self.assertEqual(self.driver.find_element(By.TAG_NAME, "h1").text, "-1")
 
     def test_multiple_increase(self):
-        driver.get(file_url("counter.html"))
-        increase = driver.find_element_by_id("increase")
-        for i in range(3):
+        self.driver.get(file_url("counter.html"))
+        increase = self.driver.find_element(By.ID, "increase")
+        for _ in range(3):
             increase.click()
-            self.assertEqual(driver.find_element_by_tag_name("h1").text, "3")
+        self.assertEqual(self.driver.find_element(By.TAG_NAME, "h1").text, "3")
 
-if __name__=="__main__":
-    unittest.main()    
+if __name__ == "__main__":
+    unittest.main()
