3cb3bc600865ba5f98d56fbfe00192777e030f6b
Bryium <bryiumonyancha@gmail.com>
2024-05-16 23:18:54 +0300

    tested the increment button

--- Test.py
+++ Test.py
@@ -19,3 +19,10 @@ class WebpageTests(unittest.TestCase):
     def test_title(self):
         driver.get(file_url("counter.html"))
         self.assertEqual(driver.title, "Counter")
+
+
+    def test_increase(self):
+        driver.get(file_url("counter.html"))
+        increase = driver.find_element_by_id("increase")
+        increase.click()
+        self.assertEqual(driver.find_element_by_tag_name("h1".text, "1"))
